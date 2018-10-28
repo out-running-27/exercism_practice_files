a5b3d800ad5a8a6817be75df2aa920c5bbd110ee
out-running-27 <paul.russum@gmail.com>
2018-10-28 09:27:02 -0500

    added new exercise

---
+++ python/allergies/allergies.py
@@ -0,0 +1,16 @@
+class Allergies(object):
+
+    def __init__(self, score):
+        self.score = score
+
+    def is_allergic_to(self, item):
+        print(self.lst)
+        pass
+
+    @property
+    def lst(self):
+        allergy_dict = {}
+        allergy_dict]"eggs"
+        allergy_list = []
+        allergy_list.append("eggs") if self.score & 1 else
+        return allergy_list
