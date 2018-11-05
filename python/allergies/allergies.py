a50aa5e8bef67971562987fc3bfc5f54167323c8
out-running-27 <paul.russum@gmail.com>
2018-11-05 06:32:08 -0600

    changed allergy_test to class constant, updated bitwise operations

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,4 +1,6 @@
 class Allergies(object):
+    ALLERGY_TEST = ["eggs", "peanuts", "shellfish", "strawberries",
+                    "tomatoes", "chocolate", "pollen", "cats"]
 
     def __init__(self, score):
         self.score = score
@@ -8,12 +10,11 @@ class Allergies(object):
 
     @property
     def lst(self):
-        allergy_test = ["eggs", "peanuts", "shellfish", "strawberries",
-                        "tomatoes", "chocolate", "pollen", "cats"]
+
         allergy_list = []
 
-        for val, allergy in enumerate(allergy_test):
-            if 2**val & self.score:
+        for val, allergy in enumerate(self.ALLERGY_TEST):
+            if 1 << val & self.score:
                 allergy_list.append(allergy)
 
         return allergy_list
