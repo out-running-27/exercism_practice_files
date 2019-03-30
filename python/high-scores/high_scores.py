cde5d07143a14481c489134b9ae3bfbb5968509f
Paul Russum <paul.russum@gmail.com>
2019-03-30 12:38:28 -0500

    completed high scores exercise

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,3 +1,27 @@
 class HighScores(object):
     def __init__(self, scores):
-        pass
+        """
+        param scores is an ordered list of integers each representing an individual score
+        """
+        self.scores = scores
+
+    def latest(self):
+        """
+        Returns int that is the last score from the list scores, None if empty
+        """
+        return self.scores[-1] if self.scores else None
+
+    def personal_best(self):
+        """
+        returns the highest integer from list scores, None if empty
+        """
+        return max(self.scores) if self.scores else None
+
+    def personal_top_three(self):
+        """
+        returns the highest three integers from list scores.
+        if less than 3 scores are available, then returns an ordered list of all elements
+        """
+        rank = min(len(self.scores), 3)
+        ranked_scores = sorted(self.scores, reverse=True)
+        return ranked_scores[:rank] if self.scores else []
