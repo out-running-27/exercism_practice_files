bdaf37673391aae67fa6225e826200822493ade4
Paul Russum <paul.russum@gmail.com>
2019-04-03 14:26:35 -0500

    completed series exercise

--- python/series/series.py
+++ python/series/series.py
@@ -1,2 +1,13 @@
 def slices(series, length):
-    pass
+
+    # length cannot be longer than the series, and must be natural number
+    if len(series) < length or length < 1:
+        raise ValueError("input not valid")
+
+    # subsets of series
+    subset = []
+
+    for i in range(len(series)-(length-1)):
+        subset.append(series[i:i+length])
+
+    return subset
