cdf32863fc39452867a97eb07f50e3e50464fb90
Paul Russum <paul.russum@gmail.com>
2019-04-04 18:02:37 -0500

    completed matrix problem and submitted for review

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,9 +1,13 @@
 class Matrix(object):
     def __init__(self, matrix_string):
-        pass
+        # divide each string my the newline character into a list
+        # each item in the list is a string representing one row
+        self.matrix_split = matrix_string.split('\n')
 
     def row(self, index):
-        pass
+        # select the row at index, and convert each element to an integer
+        return [int(i) for i in self.matrix_split[index-1].split()]
 
     def column(self, index):
-        pass
+        # loop through each row, selecting the value at the index. which creates the column
+        return [int(i.split()[index-1]) for i in self.matrix_split]
